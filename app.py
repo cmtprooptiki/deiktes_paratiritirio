50e26f6d9b75efb94cfdd16fde9fa1a5ca1acf4e
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-02-22 18:04:37 +0200

    message

--- app.py
+++ app.py
@@ -2928,7 +2928,7 @@ def main():
                         value=DS,
                         align="center",
                         number={"font": {"size": 40,"color":"#379683"}},
-                        title={"text":"Δείκτης συχνότητας<br>εργατικών ατυχημάτων<br>(ΔΣ):","font":{"size":20,"color":"gray"},"align":"center"}
+                        title={"text":"Δείκτης συχνότητας<br>εργατικών ατυχημάτων<br>(ΔΣ):","font":{"size":18,"color":"gray"},"align":"center"}
                         ))
           fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='white')
           st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
@@ -2948,7 +2948,7 @@ def main():
                         value=DE,
                         align="center",
                         number={"font": {"size": 40,"color":"#379683"}},
-                        title={"text":"Δείκτης επίπτωσης<br>εργατικών ατυχημάτων<br>(ΔΕ):","font":{"size":20,"color":"gray"},"align":"center"}
+                        title={"text":"Δείκτης επίπτωσης<br>εργατικών ατυχημάτων<br>(ΔΕ):","font":{"size":18,"color":"gray"},"align":"center"}
                         ))
           fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='white')
           st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
@@ -2968,7 +2968,7 @@ def main():
                         value=DSO,
                         align="center",
                         number={"font": {"size": 40,"color":"#379683"}},
-                        title={"text":"Δείκτης σοβαρότητας<br>εργατικών ατυχημάτων<br>(ΔΣΟ):","font":{"size":20,"color":"gray"},"align":"center"}
+                        title={"text":"Δείκτης σοβαρότητας<br>εργατικών ατυχημάτων<br>(ΔΣΟ):","font":{"size":18,"color":"gray"},"align":"center"}
                         ))
           fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='white')
           st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
@@ -2989,7 +2989,7 @@ def main():
                         value=round(apousies_percentage,1),
                         align="center",
                         number={"font": {"size": 40,"color":"#379683"}},
-                        title={"text":"Ποσοστό απουσιών<br>(%):","font":{"size":20,"color":"gray"},"align":"center"}
+                        title={"text":"Ποσοστό απουσιών<br>(%):","font":{"size":18,"color":"gray"},"align":"center"}
                         ))
           fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='white')
           st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
@@ -3009,7 +3009,7 @@ def main():
                         value=rithmos_apousiwn,
                         align="center",
                         number={"font": {"size": 40,"color":"#379683"}},
-                        title={"text":"Ρυθμός απουσιών:","font":{"size":20,"color":"gray"},"align":"center"}
+                        title={"text":"Ρυθμός απουσιών:","font":{"size":18,"color":"gray"},"align":"center"}
                         ))
           fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='white')
           st.plotly_chart(fig, use_container_width=True,config={'displayModeBar': False})
