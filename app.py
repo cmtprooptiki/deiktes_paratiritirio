22dabb0cfe900e665eb574a2465d1cad00565880
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-02-22 18:05:49 +0200

    message

--- app.py
+++ app.py
@@ -3051,18 +3051,7 @@ def main():
               val4=00.00
 
             #Call of the function
-            fig_four_cat_pie=four_cat_pie (val,val2,val3,val4)
-
-            # Customazation of the fig
-            fig_four_cat_pie.update_layout(annotations=[dict(text=str(val) + "%", x=0.052, y=0.55, font_size=30,font=dict(color="rgb(113,209,145)"), showarrow=False),
-                                            dict(text="ΤΥΠΙΚO ΣΚΟΡ", x=0.022, y=0.45, font_size=19, showarrow=False),
-                                            dict(text=str(val2) + "%", x=0.373, y=0.55, font_size=30,font=dict(color="rgba(255, 127, 14,0.8)"), showarrow=False),
-                                            dict(text="ΟΡΙΟ ΑΝΗΣΥΧ.", x=0.37, y=0.45, font_size=20, showarrow=False),
-                                            dict(text=str(val3) + "%", x=0.635, y=0.55, font_size=30,font=dict(color="rgba(255,43,43,0.8)"), showarrow=False),
-                                            dict(text="ΑΝΗΣΥΧΙΑ", x=0.63, y=0.45, font_size=20, showarrow=False),
-                                            dict(text=str(val4) + "%", x=0.955, y=0.55, font_size=30,font=dict(color="rgba(255,43,43,0.8)"), showarrow=False),
-                                            dict(text="ΣΟΒΑΡΗ<br>ΑΝΗΣΥΧ.", x=0.95, y=0.4, font_size=20, showarrow=False)
-                                            ], showlegend=False)
+            fig_four_cat_pie=four_cat_pie (val,val2,val3,val4,"ΤΥΠΙΚO ΣΚΟΡ","ΟΡΙΟ ΑΝΗΣΥΧ.","ΑΝΗΣΥΧΙΑ","ΣΟΒΑΡΗ<br>ΑΝΗΣΥΧ.")
 
             #Show the plot
             st.plotly_chart(fig_four_cat_pie, use_container_width=True,config={'displayModeBar': False})
