d88af91e2c7c78ed7fa998b987544e52dd6a1b6c
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-02-22 16:07:56 +0200

    message

--- app.py
+++ app.py
@@ -514,7 +514,7 @@ def main():
             val2=00.00
 
           #Call of the function
-          fig_two_cat_pie=two_cat_pie (val,val2,"nai","oxi")
+          fig_two_cat_pie=two_cat_pie (val,val2,"ΦΥΣΙΟΛΟΓΙΚΗ","ΠΑΘΟΛΟΓΙΚΗ")
 
           
           #Show the plot
