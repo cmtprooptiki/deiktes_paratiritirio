df2fadc33f30e51bfee17ac9012b7792ded8afda
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-02-23 12:59:18 +0200

    message

--- Deiktes_Ygeias_app_Functions.py
+++ Deiktes_Ygeias_app_Functions.py
@@ -341,7 +341,7 @@ def kpis_extra_hist(value):
                   value=round(value.mean(),1),
                   align="left",
                   number={"font": {"size": 40,"color":color}},
-                  title={"text":"Μέσος όρος","font":{"size":20,"color":"gray"},"align":"left"}
+                  title={"text":"Μ.Ο.","font":{"size":20,"color":"gray"},"align":"left"}
                   ),row=1,col=2)
     fig_extra_kpis_hist.add_trace(go.Indicator(
                   value=round(value.min(),1),
