916ec07e03b5fb838426660691109bb665cd5d2f
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-09-25 13:15:46 +0300

    message

--- app.py
+++ app.py
@@ -60,7 +60,10 @@ def main():
   #Calculation of "Hlikiakes klaseis - Eti Ekthesis" in Categorical Format:
 
   #Ηλικιακές Κλάσεις
+  st.write("prin apo EDW")
+
   df["age_group"]=np.where((df["patient_age"]>=18)&(df["patient_age"]<=34),"18-34",np.where((df["patient_age"]>=35)&(df["patient_age"]<=44),"35-44",np.where((df["patient_age"]>=45)&(df["patient_age"]<=54),"45-54",np.where((df["patient_age"]>=55)&(df["patient_age"]<=64),"55-64",np.where(df["patient_age"]>=65,"65+",np.where(df["patient_age"].isna(),np.nan,np.nan))))))
+  st.write("EFTASE EDW")
   #Έτη έκθεσης
   #ΠΡΟΣΟΧΗ: Τώρα βάζω τυχαία την στήλη date να αλλαχτεί με την σωστή όταν θα έχω την λίστα εργαζομένων:
   df["hmeromhnia_enarksis_trexousas_thesis"] = pd.to_datetime(df["hmeromhnia_enarksis_trexousas_thesis"])
