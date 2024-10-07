cb50d536675cea1b996c434af6bcc013c987dbe8
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-10-07 14:59:15 +0300

    message

--- app.py
+++ app.py
@@ -942,7 +942,7 @@ def main():
           fig_two_cat_pie=two_cat_pie (val,val2,"ΦΥΣΙΟΛΟΓΙΚΗ","ΠΑΘΟΛΟΓΙΚΗ")
 
           #Show the plot
-          st.plotly_chart(fig_two_cat_pie, use_container_width=True,config={'displayModeBar': False})
+          st.plotly_chart(fig_two_cat_pie, use_container_width=True,config={'displayModeBar': False},key="Ergastiriakes11")
           
         with col2:
         #Times gia sinartisi
