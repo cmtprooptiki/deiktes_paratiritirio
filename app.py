dc3c5bc1d4a0c167361b7a969a591e21d7f08756
Statistic1an <vintzoglou@cmtprooptiki.gr>
2024-11-29 17:24:55 +0200

    message

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ def main():
                   yref="paper",
                   x0=40,
                   y0=0,
-                  x1=max(value),
+                  x1=54,
                   y1=0.8,
                   fillcolor="green",
                   opacity=0.2,
