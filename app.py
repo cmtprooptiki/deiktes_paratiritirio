e1186def3eecf2cdcb935e6a23920afd783e8a67
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-02-11 16:02:55 +0200

    message

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
   #Calculation of "Hlikiakes klaseis - Eti Ekthesis" in Categorical Format:
 
   #Ηλικιακές Κλάσεις
-  df["age_group"]=np.where((df["patient_age"]>=18)&(df["patient_age"]<=34),"18-34",np.where((df["patient_age"]>=35)&(df["patient_age"]<=44),"35-44",np.where((df["patient_age"]>=45)&(df["patient_age"]<=54),"45-54",np.where((df["patient_age"]>=55)&(df["patient_age"]<=64),"55-64",np.where(df["patient_age"]>=65,"65+",np.where(df["patient_age"].isna(),np.nan,np.nan))))))
+  # df["age_group"]=np.where((df["patient_age"]>=18)&(df["patient_age"]<=34),"18-34",np.where((df["patient_age"]>=35)&(df["patient_age"]<=44),"35-44",np.where((df["patient_age"]>=45)&(df["patient_age"]<=54),"45-54",np.where((df["patient_age"]>=55)&(df["patient_age"]<=64),"55-64",np.where(df["patient_age"]>=65,"65+",np.where(df["patient_age"].isna(),np.nan,np.nan))))))
 
   #Έτη έκθεσης
   #ΠΡΟΣΟΧΗ: Τώρα βάζω τυχαία την στήλη date να αλλαχτεί με την σωστή όταν θα έχω την λίστα εργαζομένων:
